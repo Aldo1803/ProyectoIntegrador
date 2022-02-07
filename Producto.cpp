32343016992b84506b2c4a36377b7fd84e881b68
Aldo1803 <aldoparada1803@gmail.com>
2022-02-06 23:08:43 -0600

    Clase producto completa

--- Producto.cpp
+++ Producto.cpp
@@ -5,4 +5,28 @@ using namespace std;
 Producto::Producto(string nombre, string descripcion, double precio, int stock) {
 	m_nombre = nombre;
 	m_descripcion = descripcion;
+	m_precio = precio;
+	m_stock = stock;
+}
+
+string Producto::getName() {
+	return m_nombre;
+}
+
+string Producto::getDescription() {
+	return m_descripcion;
+}
+
+double Producto::getPrice() {
+	return m_precio;
+}
+
+int Producto::getStock() {
+	return m_stock;
+}
+
+void Producto::print() {
+	cout << "Nombre del producto: " << m_nombre << endl <<
+		"Descripcion del producto: " << m_descripcion <<
+		endl << "Precio: " << m_precio << endl << "Stock" << m_stock;
 }
